d67b0cb04c0fddff639e5973e664da8fcf088620
Kelly Hodis <kelly.hodis@processmaker.com>
2020-04-16 16:09:11 -0700

    Update Patients view

--- vue-router-api/src/views/Patients.vue
+++ vue-router-api/src/views/Patients.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>
-    <h2>
-      Patients
-    </h2>
+  <div class="patients">
+    <h1>Patients</h1>
+    <span v-bind:title="msg">
+      <input v-model="msg">
+    </span>
   </div>
 </template>
+
+<script>
+// read from api
+export default {
+  name: 'patients',
+  data () {
+    return {
+      msg: 'id'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
