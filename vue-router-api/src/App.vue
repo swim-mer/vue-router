bce7868b9570ae10637aa4bd456d721f32a28b13
Kelly Hodis <kelly.hodis@processmaker.com>
2020-04-16 12:47:16 -0700

    Add formatting

--- vue-router-api/src/App.vue
+++ vue-router-api/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/patients">Patients</router-link>
+      <router-link to="/">Home</router-link> | 
+      <router-link to="/patients">Patients</router-link> | 
       <router-link to="/orders">Orders</router-link>
     </div>
     <router-view />
