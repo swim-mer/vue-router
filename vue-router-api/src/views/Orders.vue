faecbf51166d0c2acb837b93734ad5a75f1e239f
Kelly Hodis <kelly.hodis@processmaker.com>
2020-04-17 11:06:22 -0700

    Add axios get call and restructure page

--- vue-router-api/src/views/Orders.vue
+++ vue-router-api/src/views/Orders.vue
@@ -1,7 +1,33 @@
 <template>
-  <div>
-    <h2>
-      Orders
-    </h2>
+  <div class="patients">
+    <h1>Patients</h1>
+    <p>test</p>
+    <p>{{many}}</p>
+    <p>test 2</p>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'patients',
+  data () {
+    return {
+      many: [],
+      msg: 'Enter id here'
+    }
+  },
+
+  // fetch posts when component is made
+  created() {
+    axios.get('http://api.develop.processmaker.com/api/orders').then(response => {
+        console.log(response)
+    })
+  }
+}
+
+</script>
+
+<style scoped>
+</style>
